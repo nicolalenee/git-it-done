53b05bba8be163a884605018c2e1ea9f22fa9420
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-01-03 23:39:55 -0500

    fix el declaration order in single.js

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,22 +1,22 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
-var limitWarningEl = document.querySelector("#limit-warning");
+var limitWarningEL = document.querySelector("#limit-warning");
 
 var getRepoName = function() {
   // grab repo name from url query string
   var queryString = document.location.search;
   var repoName = queryString.split("=")[1];
 
-  if (repoName) {
+  if(repoName) {
     // display repo name on the page
     repoNameEl.textContent = repoName;
-
     getRepoIssues(repoName);
-  } else {
+  }
+  else {
     // if no repo was given, redirect to the homepage
     document.location.replace("./index.html");
   }
-};
+}
 
 var getRepoIssues = function(repo) {
   // format the github api url
@@ -34,7 +34,8 @@ var getRepoIssues = function(repo) {
           displayWarning(repo);
         }
       });
-    } else {
+    }
+    else {
       // if not successful, redirect to homepage
       document.location.replace("./index.html");
     }
@@ -47,14 +48,13 @@ var displayIssues = function(issues) {
     return;
   }
 
-  // loop over given issues
   for (var i = 0; i < issues.length; i++) {
     // create a link element to take users to the issue on github
     var issueEl = document.createElement("a");
     issueEl.classList = "list-item flex-row justify-space-between align-center";
     issueEl.setAttribute("href", issues[i].html_url);
     issueEl.setAttribute("target", "_blank");
-
+    
     // create span to hold issue title
     var titleEl = document.createElement("span");
     titleEl.textContent = issues[i].title;
@@ -71,10 +71,8 @@ var displayIssues = function(issues) {
     } else {
       typeEl.textContent = "(Issue)";
     }
-
     // append to container
     issueEl.appendChild(typeEl);
-
     // append to the dom
     issueContainerEl.appendChild(issueEl);
   }
@@ -82,16 +80,15 @@ var displayIssues = function(issues) {
 
 var displayWarning = function(repo) {
   // add text to warning container
-  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+  limitWarningEL.textContent = "To see more than 30 issues, visit";
 
-  // create link element
   var linkEl = document.createElement("a");
   linkEl.textContent = "GitHub.com";
   linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
   linkEl.setAttribute("target", "_blank");
 
-  // append to warning container
-  limitWarningEl.appendChild(linkEl);
+  // append to warning container 
+  limitWarningEL.appendChild(linkEl);
 };
 
 getRepoName();
